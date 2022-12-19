a43b397a1b0a55132ae0df8c1431f2a46b5b363e
Jared <jarednuguid@gmail.com>
2022-12-19 14:10:35 -0500

    Fix to submit button

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
 const submitButton = document.getElementById("submit");
 
-submitButton.addEventListener("click", console.log(password.value));

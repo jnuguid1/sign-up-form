be786309ed44cbc0525c40d1f77d141200115739
Jared <jarednuguid@gmail.com>
2022-12-19 13:22:04 -0500

    Finalize look and basic forms

---
+++ script.js
@@ -0,0 +1,5 @@
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", console.log(password.value));
